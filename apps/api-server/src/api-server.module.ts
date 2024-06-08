31e091b233699d9577e62307718cd9aa8339715b
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-08 17:28:10 +0900

    ADD - custom logging middleware

--- apps/api-server/src/api-server.module.ts
+++ apps/api-server/src/api-server.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ApiServerController } from './api-server.controller';
 import { ApiServerService } from './api-server.service';
 import { BoardModule } from './board/board.module';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [BoardModule],
   controllers: [ApiServerController],
   providers: [ApiServerService],
 })
-export class ApiServerModule {}
+export class ApiServerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
