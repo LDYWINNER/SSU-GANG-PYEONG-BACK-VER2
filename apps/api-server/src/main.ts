c45bad4e4f2cccaa6c84998d7d853630ba43e54e
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-08 14:28:36 +0900

    SETUP - nestjs swagger

--- apps/api-server/src/main.ts
+++ apps/api-server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiServerModule } from './api-server.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiServerModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('SSU-GANG-PYEONG-BACKEND-VER.2.0')
+    .setDescription('API description for SSU-GANG-PYEONG-BACKEND-VER.2.0')
+    .setVersion('1.0')
+    .addTag('Board')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
