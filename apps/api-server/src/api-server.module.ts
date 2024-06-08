91b7320390f94f8075e88c39f6f1270c88b138cc
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-09 02:45:00 +0900

    SETUP - TypeORM

--- apps/api-server/src/api-server.module.ts
+++ apps/api-server/src/api-server.module.ts
@@ -4,9 +4,24 @@ import { ApiServerService } from './api-server.service';
 import { BoardModule } from './board/board.module';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 import ConfigModule from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule(), BoardModule],
+  imports: [
+    ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      logging: true,
+    }),
+    BoardModule,
+  ],
   controllers: [ApiServerController],
   providers: [ApiServerService],
 })
