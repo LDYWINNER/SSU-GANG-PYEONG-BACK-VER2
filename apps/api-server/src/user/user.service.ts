5e918cca3bf44d4fe22d3e9389ab281d81fad37f
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-12 00:55:19 +0900

    ADD - user signup + login logic

--- apps/api-server/src/user/user.service.ts
+++ apps/api-server/src/user/user.service.ts
@@ -1,4 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { hash, compare } from 'bcrypt';
+import { Board } from '../entity/board.entity';
+import { User } from '../entity/user.entity';
+import { Repository } from 'typeorm';
+import { LoginUserDto } from './dto/login-user.dto';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(data: CreateUserDto) {
+    const { username, name, password } = data;
+
+    const encryptedPassword = await this.encryptPassword(password);
+
+    return this.userRepository.save({
+      username,
+      name,
+      password: encryptedPassword,
+    });
+  }
+
+  async getUserForLogin(username: string) {
+    return this.userRepository
+      .createQueryBuilder('user')
+      .addSelect('user.password') // Explicitly select the password field
+      .where('user.username = :username', { username })
+      .getOne();
+  }
+
+  async login(data: LoginUserDto) {
+    const { username, password } = data;
+
+    const user = await this.getUserForLogin(username);
+
+    if (!user) throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+
+    const match = await compare(password, user.password);
+
+    if (!match)
+      throw new HttpException('UNAUTHORIZED', HttpStatus.UNAUTHORIZED);
+
+    const payload = {
+      username,
+      name: user.name,
+    };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_LIFETIME,
+    });
+
+    return {
+      accessToken,
+    };
+  }
+
+  async getUser() {
+    const qb = this.userRepository.createQueryBuilder();
+
+    qb.addSelect((subQuery) => {
+      return subQuery
+        .select('count(id)')
+        .from(Board, 'Board')
+        .where('Board.userId = User.id');
+    }, 'User_boardCount');
+
+    return qb.getMany();
+  }
+
+  async encryptPassword(password: string) {
+    const DEFAULT_SALT = 11;
+    return hash(password, DEFAULT_SALT);
+  }
+}
