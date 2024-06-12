fc82f12cfe4cbf33f3a567893b26a4342621c93b
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-12 18:07:11 +0900

    TEST - e2e initializing

--- apps/api-server/test/app.e2e-spec.ts
+++ apps/api-server/test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ApiServerModule } from './../src/api-server.module';
+import { ApiServerModule } from '../src/api-server.module';
 
-describe('ApiServerController (e2e)', () => {
+describe('e2e 테스트', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,37 @@ describe('ApiServerController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('AppController', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!');
+    });
+
+    it('/name?name=ssu (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/name?name=ssu')
+        .expect(200)
+        .expect('ssu hello');
+    });
+
+    it('[로그인]', () => {
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({
+          username: 'seederUser2',
+          password: 'hong123456',
+        })
+        .expect(201);
+    });
   });
+
+  describe('BoardController', () => {
+    it('게시글 가져오기', () => {
+      return request(app.getHttpServer()).get('/board').expect(200);
+    });
+  });
+
+  describe('UserController', () => {});
 });
