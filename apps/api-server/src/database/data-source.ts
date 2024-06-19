3de23c1673d3b7ab0f5866b0a2340a3598b7e49c
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-20 03:13:47 +0900

    CHORE

--- apps/api-server/src/database/data-source.ts
+++ apps/api-server/src/database/data-source.ts
@@ -4,19 +4,6 @@ import { SeederOptions } from 'typeorm-extension';
 
 config({ path: '.env.local' });
 
-// export default new DataSource()({
-//     type: 'postgres',
-//     host: process.env.DB_HOST,
-//     port: parseInt(process.env.DB_PORT),
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     entities: ['apps/api-server/src/**/*.entity{.ts,.js}'],
-//     migrations: ['apps/api-server/src/database/migrations/*.ts'],
-//     migrationsTableName: 'migrations',
-//     seed,
-//   });
-
 const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
