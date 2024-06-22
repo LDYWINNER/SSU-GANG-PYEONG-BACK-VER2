703df377d109cbc2234aa93a905ba7c39d7b7659
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-06-22 15:48:12 +0900

    CHORE

--- apps/api-server/src/database/migrations/1718437634083-user_timestamp.ts
+++ apps/api-server/src/database/migrations/1718437634083-user_timestamp.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UserTimestamp1718437634083 implements MigrationInterface {
-    name = 'UserTimestamp1718437634083'
+  name = 'UserTimestamp1718437634083';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "updateAt" TIMESTAMP NOT NULL DEFAULT now()`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "updateAt" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updateAt"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdAt"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updateAt"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdAt"`);
+  }
 }
--- apps/api-server/src/database/migrations/1718602291484-board_update.ts
+++ apps/api-server/src/database/migrations/1718602291484-board_update.ts
@@ -1,20 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class BoardUpdate1718602291484 implements MigrationInterface {
-    name = 'BoardUpdate1718602291484'
+  name = 'BoardUpdate1718602291484';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "board" ADD "title" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "board" ADD "views" integer NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "board" ADD "category" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "board" ADD "anonymity" boolean NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "board" ADD "title" character varying NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE "board" ADD "views" integer NOT NULL`);
+    await queryRunner.query(
+      `ALTER TABLE "board" ADD "category" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "board" ADD "anonymity" boolean NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "anonymity"`);
-        await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "category"`);
-        await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "views"`);
-        await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "title"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "anonymity"`);
+    await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "category"`);
+    await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "views"`);
+    await queryRunner.query(`ALTER TABLE "board" DROP COLUMN "title"`);
+  }
 }
