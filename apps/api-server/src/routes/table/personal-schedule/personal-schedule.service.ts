8d9358e3706ad46ac8a03ba5c804476ee4187242
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-07-02 02:22:50 +0900

    ADD - personal schedule 서비스 + dto

--- apps/api-server/src/routes/table/personal-schedule/personal-schedule.service.ts
+++ apps/api-server/src/routes/table/personal-schedule/personal-schedule.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PersonalSchedule } from '../../../entity/personal-schedule.entity';
+import { CreatePersonalScheduleDto } from './dto/create-personal-schedule.dto';
+import { UpdatePersonalScheduleDto } from './dto/update-personal-schedule.dto';
+import { Table } from '../../../entity/table.entity';
 
 @Injectable()
-export class PersonalScheduleService {}
+export class PersonalScheduleService {
+  constructor(
+    @InjectRepository(PersonalSchedule)
+    private readonly personalScheduleRepository: Repository<PersonalSchedule>,
+    @InjectRepository(Table)
+    private readonly tableRepository: Repository<Table>,
+  ) {}
+
+  createPersonalSchedule = async (
+    tableId: string,
+    createPersonalScheduleDto: CreatePersonalScheduleDto,
+  ) => {
+    const table = await this.tableRepository.findOne({
+      where: { id: tableId },
+    });
+    if (!table) {
+      throw new NotFoundException(`Table with ID ${tableId} not found`);
+    }
+
+    const newPersonalSchedule = this.personalScheduleRepository.create({
+      tableEntity: table,
+      table: tableId,
+      ...createPersonalScheduleDto,
+    });
+
+    return this.personalScheduleRepository.save(newPersonalSchedule);
+  };
+
+  updatePersonalSchedule = async (
+    personalScheduleId: string,
+    newPersonalSchedule: UpdatePersonalScheduleDto,
+  ) => {
+    const personalSchedule = await this.personalScheduleRepository.findOne({
+      where: { id: personalScheduleId },
+    });
+    if (!personalSchedule) {
+      throw new NotFoundException(
+        `PersonalSchedule with ID ${personalScheduleId} not found`,
+      );
+    }
+
+    await this.personalScheduleRepository.update(personalScheduleId, {
+      ...newPersonalSchedule,
+    });
+
+    return this.personalScheduleRepository.findOne({
+      where: { id: personalScheduleId },
+      relations: ['table'],
+    });
+  };
+
+  deletePersonalSchedule = async (personalScheduleId: string) => {
+    const personalSchedule = await this.personalScheduleRepository.findOne({
+      where: { id: personalScheduleId },
+    });
+    if (!personalSchedule) {
+      throw new NotFoundException(
+        `PersonalSchedule with ID ${personalScheduleId} not found`,
+      );
+    }
+
+    await this.personalScheduleRepository.remove(personalSchedule);
+
+    return personalSchedule;
+  };
+}
