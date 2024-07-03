7de2f9414fbc291027ae0f19a5c7c16497e08b06
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-07-03 16:19:14 +0900

    MIGRATION - personal schedule

--- apps/api-server/src/database/migrations/index.ts
+++ apps/api-server/src/database/migrations/index.ts
@@ -1,9 +1,11 @@
 import { SsuGangPyeongReference1719681701078 } from './1719681701078-ssu-gang-pyeong-reference';
 import { TablePersonalSchedule1719813960356 } from './1719813960356-table-personal-schedule';
 import { TableSchoolSchedule1719894904035 } from './1719894904035-table-school-schedule';
+import { TablePersonalSchedule1719942971413 } from './1719942971413-table-personal-schedule';
 
 export {
   SsuGangPyeongReference1719681701078,
   TablePersonalSchedule1719813960356,
   TableSchoolSchedule1719894904035,
+  TablePersonalSchedule1719942971413,
 };
