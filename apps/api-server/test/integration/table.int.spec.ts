f09bb485d564a467aa4a2bd7c7a59654a3156827
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-07-04 16:49:32 +0900

    TEST:REFACTOR - table 통합테스트
    personal schedule이랑 하나의 describe에서 테스트

--- apps/api-server/test/integration/table.int.spec.ts
+++ apps/api-server/test/integration/table.int.spec.ts
@@ -17,6 +17,7 @@ describe('Table 기능 통합 테스트', () => {
   let tableRepository: Repository<Table>;
   let userRepository: Repository<User>;
   let refreshTokenRepository: Repository<RefreshToken>;
+  let personalScheduleRepository: Repository<PersonalSchedule>;
   let userId: string;
   let token: string;
   let invalidToken: string;
@@ -50,6 +51,9 @@ describe('Table 기능 통합 테스트', () => {
     refreshTokenRepository = moduleFixture.get<Repository<RefreshToken>>(
       getRepositoryToken(RefreshToken),
     );
+    personalScheduleRepository = moduleFixture.get<
+      Repository<PersonalSchedule>
+    >(getRepositoryToken(PersonalSchedule));
 
     // 테스트용 사용자 생성
     const user = userRepository.create({
@@ -78,6 +82,7 @@ describe('Table 기능 통합 테스트', () => {
   afterAll(async () => {
     // 테스트 이후에 생성된 데이터 정리
     await tableRepository.delete({});
+    await personalScheduleRepository.delete({});
     await refreshTokenRepository.delete({});
     await userRepository.delete({});
     await app.close();
@@ -214,119 +219,85 @@ describe('Table 기능 통합 테스트', () => {
       expect(response.status).toBe(404);
     });
   });
-});
 
-describe('시간표 Personal Schedule 기능 통합 테스트', () => {
-  let app: INestApplication;
-  let tableRepository: Repository<Table>;
-  let personalScheduleRepository: Repository<PersonalSchedule>;
-  let userRepository: Repository<User>;
-  let refreshTokenRepository: Repository<RefreshToken>;
-  let userId: string;
-  let tableId: string;
-  let token: string;
-  let invalidToken: string;
+  describe('시간표 Personal Schedule 기능 통합 테스트', () => {
+    let tableId: string;
 
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        ApiServerModule,
-        TableModule,
-        UserModule,
-        JwtModule.registerAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: (configService: ConfigService) => ({
-            secret: configService.get<string>('jwt.secret'),
-            signOptions: { expiresIn: '60m' },
+    beforeAll(async () => {
+      // table 초기 설정
+      const table = tableRepository.create({
+        title: 'ps_test_table',
+        user: { id: userId },
+      });
+      await tableRepository.save(table);
+      tableId = table.id;
+    });
+
+    describe('/personal-schedule (POST)', () => {
+      afterEach(async () => {
+        await personalScheduleRepository.delete({});
+      });
+
+      it('personal schedule 생성 테스트', async () => {
+        const response = await request(app.getHttpServer())
+          .post('/personal-schedule')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            tableId: tableId,
+            courseId: 'course_id',
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+            },
+          });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            courseId: 'course_id',
+            tableTitle: 'ps_test_table',
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+            },
           }),
-        }),
-      ],
-    }).compile();
+        );
+      });
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-
-    tableRepository = moduleFixture.get<Repository<Table>>(
-      getRepositoryToken(Table),
-    );
-    userRepository = moduleFixture.get<Repository<User>>(
-      getRepositoryToken(User),
-    );
-    refreshTokenRepository = moduleFixture.get<Repository<RefreshToken>>(
-      getRepositoryToken(RefreshToken),
-    );
-    personalScheduleRepository = moduleFixture.get<
-      Repository<PersonalSchedule>
-    >(getRepositoryToken(PersonalSchedule));
-
-    // 테스트용 사용자 생성
-    const user = userRepository.create({
-      username: 'test_ps_user',
-      email: 'test_ps_user@example.com',
-      password: 'test_password',
-      postCount: 0,
-      role: Role.User,
-    });
-    await userRepository.save(user);
-    userId = user.id;
-
-    // JWT 토큰 발급
-    const jwtService = moduleFixture.get<JwtService>(JwtService);
-    token = jwtService.sign({ id: userId, username: user.username });
-    // Invalid JWT 토큰 발급
-    const invalidSecret = 'some-invalid-secret'; // Different from your actual secret
-    const invalidPayload = {
-      id: 'invalid-uuid', // Simulate an invalid user id
-      username: 'invalidUser',
-      email: 'invalid@example.com',
-    };
-    invalidToken = jwtService.sign(invalidPayload, { secret: invalidSecret });
-
-    // table 초기 설정
-    const table = tableRepository.create({
-      title: 'ps_test_table',
-      user: { id: userId },
-    });
-    await tableRepository.save(table);
-    tableId = table.id;
-    console.log(tableId);
-  });
-
-  afterAll(async () => {
-    // 테스트 이후에 생성된 데이터 정리
-    await tableRepository.delete({});
-    await personalScheduleRepository.delete({});
-    await refreshTokenRepository.delete({});
-    await userRepository.delete({});
-    await app.close();
-  });
-
-  describe('/personal-schedule (POST)', () => {
-    afterEach(async () => {
-      await personalScheduleRepository.delete({});
-    });
-
-    it('personal schedule 생성 테스트', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/personal-schedule')
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          tableId: tableId,
-          courseId: 'course_id',
-          sections: {
-            section1: {
-              days: [1, 3],
-              startTimes: ['10:00'],
-              endTimes: ['11:00'],
-              locations: ['room1'],
+      it('유효하지 않은 토큰으로 요청을 보내는 경우 401를 반환해야 합니다', async () => {
+        const response = await request(app.getHttpServer())
+          .post('/personal-schedule')
+          .set('Authorization', `Bearer ${invalidToken}`)
+          .send({
+            tableId,
+            courseId: 'course_id',
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
             },
-          },
-        });
+          });
 
-      expect(response.status).toBe(201);
-      expect(response.body).toEqual(
-        expect.objectContaining({
+        expect(response.status).toBe(401);
+      });
+    });
+
+    describe('/personal-schedule/:id (PUT)', () => {
+      let personalScheduleId: string;
+
+      beforeEach(async () => {
+        const personalSchedule = personalScheduleRepository.create({
           courseId: 'course_id',
           tableTitle: 'ps_test_table',
           sections: {
@@ -337,204 +308,136 @@ describe('시간표 Personal Schedule 기능 통합 테스트', () => {
               locations: ['room1'],
             },
           },
-        }),
-      );
-    });
-
-    it('유효하지 않은 토큰으로 요청을 보내는 경우 401를 반환해야 합니다', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/personal-schedule')
-        .set('Authorization', `Bearer ${invalidToken}`)
-        .send({
-          tableId,
-          courseId: 'course_id',
-          sections: {
-            section1: {
-              days: [1, 3],
-              startTimes: ['10:00'],
-              endTimes: ['11:00'],
-              locations: ['room1'],
-            },
-          },
         });
 
-      expect(response.status).toBe(401);
-    });
-  });
-
-  describe('/personal-schedule/:id (PUT)', () => {
-    let personalScheduleId: string;
-
-    beforeEach(async () => {
-      const personalSchedule = personalScheduleRepository.create({
-        courseId: 'course_id',
-        tableTitle: 'ps_test_table',
-        sections: {
-          section1: {
-            days: [1, 3],
-            startTimes: ['10:00'],
-            endTimes: ['11:00'],
-            locations: ['room1'],
-          },
-        },
-      });
-
-      await personalScheduleRepository.save(personalSchedule);
-      personalScheduleId = personalSchedule.id;
-    });
-
-    afterEach(async () => {
-      await personalScheduleRepository.delete({});
-    });
-
-    it('스케줄 제목(courseId) 수정 테스트', async () => {
-      const response = await request(app.getHttpServer())
-        .put(`/personal-schedule/${personalScheduleId}`)
-        .set('Authorization', `Bearer ${token}`)
-        .send({ courseId: 'updated_course_id' });
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          id: personalScheduleId,
-          courseId: 'updated_course_id',
-        }),
-      );
-    });
-
-    it('스케줄 sections 수정 테스트', async () => {
-      const response = await request(app.getHttpServer())
-        .put(`/personal-schedule/${personalScheduleId}`)
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          sections: {
-            section1: {
-              days: [1, 3],
-              startTimes: ['10:00'],
-              endTimes: ['11:00'],
-              locations: ['room1'],
-            },
-            sections2: {
-              days: [2, 4],
-              startTimes: ['11:00'],
-              endTimes: ['12:00'],
-              locations: ['room2'],
-            },
-          },
-        });
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          id: personalScheduleId,
-          sections: {
-            section1: {
-              days: [1, 3],
-              startTimes: ['10:00'],
-              endTimes: ['11:00'],
-              locations: ['room1'],
-            },
-            sections2: {
-              days: [2, 4],
-              startTimes: ['11:00'],
-              endTimes: ['12:00'],
-              locations: ['room2'],
-            },
-          },
-        }),
-      );
-    });
-
-    it('스케줄 제목(courseId) & sections 동시 수정 테스트', async () => {
-      const response = await request(app.getHttpServer())
-        .put(`/personal-schedule/${personalScheduleId}`)
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          courseId: 'updated_course_id',
-          sections: {
-            section1: {
-              days: [1, 3],
-              startTimes: ['10:00'],
-              endTimes: ['11:00'],
-              locations: ['room1'],
-            },
-            sections2: {
-              days: [2, 4],
-              startTimes: ['11:00'],
-              endTimes: ['12:00'],
-              locations: ['room2'],
-            },
-          },
-        });
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          id: personalScheduleId,
-          courseId: 'updated_course_id',
-          sections: {
-            section1: {
-              days: [1, 3],
-              startTimes: ['10:00'],
-              endTimes: ['11:00'],
-              locations: ['room1'],
-            },
-            sections2: {
-              days: [2, 4],
-              startTimes: ['11:00'],
-              endTimes: ['12:00'],
-              locations: ['room2'],
-            },
-          },
-        }),
-      );
-    });
-
-    it('유효하지 않은 Personal Schedule 아이디로 수정 요청을 보내면 404를 반환해야 합니다', async () => {
-      const invalidPersonalScheduleId = uuidv4();
-
-      const response = await request(app.getHttpServer())
-        .put(`/personal-schedule/${invalidPersonalScheduleId}`)
-        .set('Authorization', `Bearer ${token}`)
-        .send({ courseId: 'updated_course_id' });
-
-      expect(response.status).toBe(404);
-    });
-  });
-
-  describe('/personal-schedule/:id (DELETE)', () => {
-    let personalScheduleId: string;
-
-    beforeEach(async () => {
-      const personalSchedule = personalScheduleRepository.create({
-        courseId: 'course_id',
-        tableTitle: 'ps_test_table',
-        sections: {
-          section1: {
-            days: [1, 3],
-            startTimes: ['10:00'],
-            endTimes: ['11:00'],
-            locations: ['room1'],
-          },
-        },
-      });
-      const savedPersonalSchedule =
         await personalScheduleRepository.save(personalSchedule);
-      personalScheduleId = savedPersonalSchedule.id;
+        personalScheduleId = personalSchedule.id;
+      });
+
+      afterEach(async () => {
+        await personalScheduleRepository.delete({});
+      });
+
+      it('스케줄 제목(courseId) 수정 테스트', async () => {
+        const response = await request(app.getHttpServer())
+          .put(`/personal-schedule/${personalScheduleId}`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({ courseId: 'updated_course_id' });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: personalScheduleId,
+            courseId: 'updated_course_id',
+          }),
+        );
+      });
+
+      it('스케줄 sections 수정 테스트', async () => {
+        const response = await request(app.getHttpServer())
+          .put(`/personal-schedule/${personalScheduleId}`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+              sections2: {
+                days: [2, 4],
+                startTimes: ['11:00'],
+                endTimes: ['12:00'],
+                locations: ['room2'],
+              },
+            },
+          });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: personalScheduleId,
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+              sections2: {
+                days: [2, 4],
+                startTimes: ['11:00'],
+                endTimes: ['12:00'],
+                locations: ['room2'],
+              },
+            },
+          }),
+        );
+      });
+
+      it('스케줄 제목(courseId) & sections 동시 수정 테스트', async () => {
+        const response = await request(app.getHttpServer())
+          .put(`/personal-schedule/${personalScheduleId}`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            courseId: 'updated_course_id',
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+              sections2: {
+                days: [2, 4],
+                startTimes: ['11:00'],
+                endTimes: ['12:00'],
+                locations: ['room2'],
+              },
+            },
+          });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: personalScheduleId,
+            courseId: 'updated_course_id',
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+              sections2: {
+                days: [2, 4],
+                startTimes: ['11:00'],
+                endTimes: ['12:00'],
+                locations: ['room2'],
+              },
+            },
+          }),
+        );
+      });
+
+      it('유효하지 않은 Personal Schedule 아이디로 수정 요청을 보내면 404를 반환해야 합니다', async () => {
+        const invalidPersonalScheduleId = uuidv4();
+
+        const response = await request(app.getHttpServer())
+          .put(`/personal-schedule/${invalidPersonalScheduleId}`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({ courseId: 'updated_course_id' });
+
+        expect(response.status).toBe(404);
+      });
     });
 
-    afterEach(async () => {
-      await personalScheduleRepository.delete({});
-    });
+    describe('/personal-schedule/:id (DELETE)', () => {
+      let personalScheduleId: string;
 
-    it('personal schedule 삭제 테스트', async () => {
-      const response = await request(app.getHttpServer())
-        .delete(`/personal-schedule/${personalScheduleId}`)
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual(
-        expect.objectContaining({
+      beforeEach(async () => {
+        const personalSchedule = personalScheduleRepository.create({
           courseId: 'course_id',
           tableTitle: 'ps_test_table',
           sections: {
@@ -545,23 +448,52 @@ describe('시간표 Personal Schedule 기능 통합 테스트', () => {
               locations: ['room1'],
             },
           },
-        }),
-      );
-      const deletedPersonalSchedule =
-        await personalScheduleRepository.findOneBy({
-          id: personalScheduleId,
         });
-      expect(deletedPersonalSchedule).toBeNull();
-    });
+        const savedPersonalSchedule =
+          await personalScheduleRepository.save(personalSchedule);
+        personalScheduleId = savedPersonalSchedule.id;
+      });
 
-    it('유효하지 않은 테이블 아이디로 삭제 요청을 보내면 404를 반환해야 합니다', async () => {
-      const invalidPersonalScheduleId = uuidv4();
+      afterEach(async () => {
+        await personalScheduleRepository.delete({});
+      });
 
-      const response = await request(app.getHttpServer())
-        .delete(`/personal-schedule/${invalidPersonalScheduleId}`)
-        .set('Authorization', `Bearer ${token}`);
+      it('personal schedule 삭제 테스트', async () => {
+        const response = await request(app.getHttpServer())
+          .delete(`/personal-schedule/${personalScheduleId}`)
+          .set('Authorization', `Bearer ${token}`);
 
-      expect(response.status).toBe(404);
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            courseId: 'course_id',
+            tableTitle: 'ps_test_table',
+            sections: {
+              section1: {
+                days: [1, 3],
+                startTimes: ['10:00'],
+                endTimes: ['11:00'],
+                locations: ['room1'],
+              },
+            },
+          }),
+        );
+        const deletedPersonalSchedule =
+          await personalScheduleRepository.findOneBy({
+            id: personalScheduleId,
+          });
+        expect(deletedPersonalSchedule).toBeNull();
+      });
+
+      it('유효하지 않은 테이블 아이디로 삭제 요청을 보내면 404를 반환해야 합니다', async () => {
+        const invalidPersonalScheduleId = uuidv4();
+
+        const response = await request(app.getHttpServer())
+          .delete(`/personal-schedule/${invalidPersonalScheduleId}`)
+          .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(404);
+      });
     });
   });
 });
