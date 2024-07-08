8ec29dac26039f75720463bbac7488d907020320
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-07-08 23:38:55 +0900

    UPDATE - task stub 레포지토리 find 자세한 구현

--- apps/api-server/test/unit/todo/stub/task-repository.ts
+++ apps/api-server/test/unit/todo/stub/task-repository.ts
@@ -28,11 +28,45 @@ export class StubTaskRepository {
   }
 
   find(conditions: any): Promise<ToDoTask[]> {
-    return Promise.resolve(
-      this.toDoTasks.filter(
-        (toDoTask) => toDoTask.user.id === conditions.where.user.id,
-      ),
-    );
+    if (conditions.where.toDoCategory !== undefined) {
+      return Promise.resolve(
+        this.toDoTasks.filter(
+          (toDoTask) =>
+            toDoTask.toDoCategory.id === conditions.where.toDoCategory.id,
+        ),
+      );
+    } else if (
+      conditions.where.user !== undefined &&
+      conditions.where.isCompleted !== undefined
+    ) {
+      return Promise.resolve(
+        this.toDoTasks.filter(
+          (toDoTask) =>
+            toDoTask.user.id === conditions.where.user.id &&
+            toDoTask.isCompleted === conditions.where.isCompleted,
+        ),
+      );
+    } else if (
+      conditions.where.user !== undefined &&
+      conditions.where.completeDate !== undefined
+    ) {
+      // Simulate Like functionality for completeDate
+      const yearMonthPattern = conditions.where.completeDate._value;
+
+      return Promise.resolve(
+        this.toDoTasks.filter(
+          (toDoTask) =>
+            toDoTask.user.id === conditions.where.user.id &&
+            toDoTask.completeDate.startsWith(yearMonthPattern),
+        ),
+      );
+    } else if (conditions.where.user !== undefined) {
+      return Promise.resolve(
+        this.toDoTasks.filter(
+          (toDoTask) => toDoTask.user.id === conditions.where.user.id,
+        ),
+      );
+    }
   }
 
   async update(id: string, newToDoTask: Partial<ToDoTask>): Promise<any> {
