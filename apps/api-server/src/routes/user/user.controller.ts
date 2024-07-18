e29b32d99879d1a15c1fdd28c21ea72f34dc4c1a
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-07-19 04:51:19 +0900

    ADD - 유저 팔로우 기능 컨트롤러 구현

--- apps/api-server/src/routes/user/user.controller.ts
+++ apps/api-server/src/routes/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  InternalServerErrorException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiGetItemsResponse } from '../../common/decorators/swagger.decorator';
@@ -6,6 +14,10 @@ import { FindUserResDto } from './dto/res.dto';
 import { Roles } from '../../common/decorators/role.decorator';
 import { UserType } from '../../common/enum/user.enum';
 import { PageReqDto } from '../../common/dto/page-request.dto';
+import {
+  UserAfterAuth,
+  UserInfo,
+} from '../../common/decorators/user-info.decorator';
 
 @Controller('user')
 @ApiTags('User')
@@ -28,5 +40,58 @@ export class UserController {
         postCount,
       };
     });
+  }
+
+  @Post('follow/:followerId')
+  async createFollow(
+    @UserInfo() userInfo: UserAfterAuth,
+    @Param('followerId') followerId: string,
+  ) {
+    try {
+      const userId = userInfo.id;
+      const result = await this.userService.createFollow(userId, followerId);
+      if (!result) {
+        throw new InternalServerErrorException(
+          `Failed to create follow for leader id: ${userId} & follower id: ${followerId}`,
+        );
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get('follow/:id')
+  async getFollower(@Param('id') id: string) {
+    try {
+      const result = await this.userService.getFollower(id);
+      if (!result) {
+        throw new InternalServerErrorException(
+          `Failed to get follower with id ${id}`,
+        );
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete('follow/:followerId')
+  async deleteFollow(
+    @UserInfo() userInfo: UserAfterAuth,
+    @Param('followerId') followerId: string,
+  ) {
+    try {
+      const userId = userInfo.id;
+      const result = await this.userService.removeFollow(userId, followerId);
+      if (!result) {
+        throw new InternalServerErrorException(
+          `Failed to delete follow for leader id: ${userId} & follower id: ${followerId}`,
+        );
+      }
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 }
