676d9c0890082e23da81f5cc3fbc624111e979a3
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-08-01 23:54:39 +0900

    CHORE

--- apps/api-server/src/config/redis.config.ts
+++ apps/api-server/src/config/redis.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('redis', () => ({
   host: process.env.REDIS_HOST || 'redis',
-  port: process.env.REDIS_PORT || 6379,
+  port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
 }));
